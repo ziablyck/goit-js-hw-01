ab32ba162e2373e37524d68f4c1f68e05dbd70c8
ziablyck <monechca@gmail.com>
2023-12-10 19:52:48 +0200

    hw-1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function makeTransaction(quantity, pricePerDroid) {
+    return `"You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!"`
+}
+makeTransaction(5, 3000)
+makeTransaction(3, 1000)
+makeTransaction(10, 500)
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
